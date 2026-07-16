e46bdabbdcfbeb98a82e73b1d00eef6f7c47eaf3
David Terei <code@davidterei.com>
2026-07-16 13:20:03 -0400

    Add a sentinel error value, AnError, for convenience

--- assert/lib.go
+++ assert/lib.go
@@ -3,9 +3,15 @@ package assert
 //go:generate ../internal/codegen/gen.sh --pkg=assert
 
 import (
+	"errors"
+
 	"github.com/rubrikinc/testwell/internal/fail"
 	"github.com/rubrikinc/testwell/testing"
 )
+
+// AnError is a sentinel error for use in tests that need an error value but
+// don't care about its contents.
+var AnError = errors.New("testwell.AnError general error for testing")
 
 func failTest(t testing.T, tf fail.TestFailure) bool {
 	t.Helper()
--- expect/lib.go
+++ expect/lib.go
@@ -3,9 +3,15 @@ package expect
 //go:generate ../internal/codegen/gen.sh --pkg=expect
 
 import (
+	"errors"
+
 	"github.com/rubrikinc/testwell/internal/fail"
 	"github.com/rubrikinc/testwell/testing"
 )
+
+// AnError is a sentinel error for use in tests that need an error value but
+// don't care about its contents.
+var AnError = errors.New("testwell.AnError general error for testing")
 
 func failTest(t testing.T, tf fail.TestFailure) bool {
 	t.Helper()
